db9677f696627fc80ec083c24f2050e6849e4c2d
khaalid subaan <khaalidsubaan@gmail.com>
2021-02-05 16:47:46 +0800

    initial graphql installation and configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GraphQLModule.forRoot({}), PostsModule],
 })
 export class AppModule {}
